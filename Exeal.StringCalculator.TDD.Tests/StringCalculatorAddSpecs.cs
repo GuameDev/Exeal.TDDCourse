bb77e3e6e52ac02ece600c85cc6a7173fd4b9040
Pablo Muñoz <pablommartinezdev@gmail.com>
2024-07-10 12:53:18 +0200

    feat: create string calculator project and make first unit test

---
+++ Exeal.StringCalculator.TDD.Tests/StringCalculatorAddSpecs.cs
@@ -0,0 +1,17 @@
+namespace Exeal.StringCalculator.TDD.Tests
+{
+    public class StringCalculatorAddSpecs
+    {
+        [Theory]
+        [InlineData("3,5", 8)]
+        [InlineData("4,5", 9)]
+        [InlineData("5,5", 10)]
+        [InlineData("5,5,5", 15)]
+        [InlineData("1,2,3", 6)]
+        public void Add_GivenAnyNumbersReturnSum(string numbers, int expectedResult)
+        {
+            var result = StringCalculator.Add(numbers);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ Exeal.StringCalculator.TDD/StringCalculator.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Exeal.StringCalculator.TDD
+{
+    public class StringCalculator
+    {
+        public static int Add(string numbers)
+        {
+            return numbers.Split(',').Select(x => Convert.ToInt32(x)).Sum(x => x);
+        }
+    }
+}
